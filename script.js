b59fb520998221bd8c17f92d4aa233afb9f5ec9e
Vskelite <khamkarv01@gmail.com>
2024-09-25 22:10:22 +0530

    JS updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const searchButton = document.querySelector("submit-btn");
+document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.querySelector(".submit-btn");
     const userNameInput = document.getElementById("username");
     const statsContainer = document.querySelector(".stats-container");
     const easyProgressCircle = document.querySelector(".easy-progress");
@@ -11,45 +11,70 @@ document.addEventListener('DOMContentLoaded', function(){
     const cardStatsContainer = document.querySelector(".stats-cards");
 
 
-    function validateUsername(username){
-        if(username.trim() === ""){
+    function validateUsername(username) {
+        if (username.trim() === "") {
             alert("Usernme should not br empty");
             return false;
         }
         const regex = /^[a-zA-Z0-9_-]{1,15}$/;
         const isMatching = regex.test(username);
-        if(isMatching){
+        if (!isMatching) {
             alert("Invalid username");
         }
         return isMatching;
     }
 
     async function fetchUserDetails(username) {
-        const url = `https://leetcode-stats-api.herokuapp.com/${username}}`
-        try{
+
+
+
+
+        try {
 
             searchButton.textContent = "Searching...";
             searchButton.disabled = true;
-            const response = await fetch(url);
-            if(!response.ok){
+
+
+            const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+            const targetUrl = 'https://leetcode.com/graphql/';
+
+            const myHeaders = new Headers();
+            myHeaders.append("content-type", "application/json");
+
+            const graphql = JSON.stringify({
+                query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+                variables: { "username": `${username}` }
+            })
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body: graphql,
+                redirect: "follow"
+            };
+
+            const response = await fetch(proxyUrl+targetUrl, requestOptions);
+
+
+            if (!response.ok) {
                 throw new Error("Unable to fetch the User details");
             }
 
             const data = await response.json();
             console.log("Logging data: ", data);
         }
-        catch(error){
-
+        catch (error) {
+            statsContainer.innerHTML = `<p>No data found</p>`
         }
-        finally{
-
+        finally {
+            searchButton.textContent = "Search";
+            searchButton.disabled = false;
         }
     }
-    searchButton.addEventListener("click", function(){
+    searchButton.addEventListener('click', function () {
         const username = userNameInput.value;
         console.log("Logging username: ", username);
-        if(validateUsername(username)){
-             fetchUserDetails(username);
+        if (validateUsername(username)) {
+            fetchUserDetails(username);
         }
     })
 })
